4ecb3d603834e446154b4ac393188bed7a459f1d
Marcin Trocha <cinekmar21@gmail.com>
2019-04-22 11:33:56 +0200

    Added Sorting

--- mikroERP-SPA/src/app/employees-table/employees-table.component.ts
+++ mikroERP-SPA/src/app/employees-table/employees-table.component.ts
@@ -2,7 +2,7 @@
 import {Component, OnInit, HostListener, ViewChild} from '@angular/core';
 import { Employee } from '../_models/employee';
 import { EmployeeService } from '../_services/employee.service';
-import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { EmbeddedTemplateAst } from '@angular/compiler';
 import { DataSource } from '@angular/cdk/table';
 
@@ -13,16 +13,18 @@ import { DataSource } from '@angular/cdk/table';
 })
 
 export class EmployeesTableComponent implements OnInit {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   employees: Employee[];
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'sex', 'wages', 'dayOfEmployment',
    'phone', 'dateOfBirth', 'nameOfDepartment', 'roomNr', 'floorNr', 'nameOfTransport', 'email'];
   public dataSource = new MatTableDataSource<Employee>();
   constructor(private employeeService: EmployeeService) { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
    this.getEmployees();
    this.dataSource.paginator = this.paginator;
+   this.dataSource.sort = this.sort;
   }
 
   public getEmployees = () => {
