54c60608b1ccb0c82c8aedefc892a4044a523b2c
Marcin Trocha <cinekmar21@gmail.com>
2019-04-23 22:03:31 +0200

    Department Chart Ready

--- mikroERP-SPA/src/app/Charts/department-chart/department-chart.component.ts
+++ mikroERP-SPA/src/app/Charts/department-chart/department-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartService } from 'src/app/_services/chart.service';
 import { DepartmetCount } from 'src/app/_models/departmetCount';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-department-chart',
@@ -17,6 +18,42 @@ export class DepartmentChartComponent implements OnInit {
   getDepartmentCount() {
     this.chartService.getDepartmentCount().subscribe((departmentCount: DepartmetCount) => {
       this.departmentCount = departmentCount;
+      this.BarChart = new Chart('barChart', {
+        type: 'bar',
+      data: {
+       labels: ['Management', 'Logistics Department', 'Administration Department', 'Marketing department'],
+       datasets: [{
+           label: '# of Votes',
+           data: [9, 7, 3, 4],
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255,99,132,1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)'
+           ],
+           borderWidth: 1
+       }]
+      },
+      options: {
+       title: {
+           text: 'Department Chart',
+           display: true
+       },
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero: true
+               }
+           }]
+       }
+      }
+      });
     }, error => {
       console.log(error);
     });
