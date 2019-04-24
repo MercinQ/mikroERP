ef54b8c531e99c7203cc5e0146669f0593be4e55
Marcin Trocha <cinekmar21@gmail.com>
2019-04-24 22:08:56 +0200

    EmployedInMonthDto Added

---
+++ mikroERP.API/Dtos/NumberOfEmployedInMonthDto.cs
@@ -0,0 +1,18 @@
+namespace mikroERP.API.Dtos
+{
+    public class NumberOfEmployedInMonthDto
+    {
+        public int EmployedInJan { get; set; }
+        public int EmployedInFeb { get; set; }
+        public int EmployedInMarch { get; set; }
+        public int EmployedInApril { get; set; }
+        public int EmployedInMay { get; set; }
+        public int EmployedInJune { get; set; }
+        public int EmployedInJuly { get; set; }
+        public int EmployedInAug { get; set; }
+        public int EmployedInSep { get; set; }
+        public int EmployedInOct { get; set; }
+        public int EmployedInNov { get; set; }
+        public int EmployedInDec { get; set; }
+    }
+}
